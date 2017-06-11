5b2b92f29351ce2005d2461b199f4470ea0e9d10
Stas Maksin <stasm@traffilog.com>
2017-06-11 15:26:45 +0300

    added window_origin

--- js/iframeApp.js
+++ js/iframeApp.js
@@ -1,25 +1,26 @@
 window.addEventListener("message", handleReceivedMessage, false);
 
 var buttonFromChild = document.getElementById('buttonFromChild');
+var WINDOW_ORIGIN = window.location.origin;
 
 function handleReceivedMessage(_e) {
 	try {
-		if(_e.origin != "http://localhost:8080") {
-			return
+		if(_e.origin != WINDOW_ORIGIN) {
+			return;
 		}
 		console.log(_e.data)
-		var messageTxt = document.createElement('div')
+		var messageTxt = document.createElement('div');
 		messageTxt.innerText =  _e.data;
-		document.getElementById('receivedMessageChild').appendChild(messageTxt)
+		document.getElementById('receivedMessageChild').appendChild(messageTxt);
 
 	} catch(err){ 
-		console.log('handleReceivedMessage: ' + err)
+		console.log('handleReceivedMessage: ' + err);
 	}
 }
 
 function sendMessageToParentFrame(_e) {
 	try{
-		window.parent.postMessage("test message from child", "http://localhost:8080")
+		window.parent.postMessage("test message from child", WINDOW_ORIGIN);
 	} catch(err) {
 		console.log('sendMessageToParentFrame: ' + err);
 	}
--- js/index.js
+++ js/index.js
@@ -1,26 +1,27 @@
 window.addEventListener("message", handleReceivedMessage, false);
-
+console.log(window.location.origin)
 var buttonFromParent = document.getElementById('buttonFromParent');
+var WINDOW_ORIGIN = window.location.origin;
 
 function handleReceivedMessage(_e) {
 	try {
-		if(_e.origin != "http://localhost:8080") {
-			return
+		if(_e.origin != WINDOW_ORIGIN) {
+			return;
 		}
-		console.log(_e.data)
-		var messageTxt = document.createElement('div')
+		console.log(_e.data);
+		var messageTxt = document.createElement('div');
 		messageTxt.innerText =  _e.data;
-		document.getElementById('receivedMessageParent').appendChild(messageTxt)
+		document.getElementById('receivedMessageParent').appendChild(messageTxt);
 
 	} catch(err){ 
-		console.log('handleReceivedMessage: ' + err)
+		console.log('handleReceivedMessage: ' + err);
 	}
 }
 
 function sendMessageToChildFrame(_e) {
 	try{
 		var iframe = document.getElementById('iframe').contentWindow;
-		iframe.postMessage("test message from parent", "http://localhost:8080/iframe.html")
+		iframe.postMessage("test message from parent", WINDOW_ORIGIN)
 	} catch(err) {
 		console.log('sendMessageToParentFrame: ' + err);
 	}
